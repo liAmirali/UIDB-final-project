d59d20e1cebeff62350947546942a67783c86b31
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-01-25 18:53:49 +0330

    Move backend to a separate folder

--- routes/shops.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-shops_blueprint = Blueprint("shops_blueprint", __name__)
-
-@shops_blueprint.route('/shops')
-def hello_world():
-    return 'Hello, World!'
---
+++ server/routes/shops.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, jsonify
+
+shops_blueprint = Blueprint("shops_blueprint", __name__)
+
+@shops_blueprint.route('/shops')
+def hello_world():
+    json_data = {
+        "name": "John Doe",
+        "age": 30,
+        "city": "Example City"
+    }
+
+    return jsonify(json_data)
