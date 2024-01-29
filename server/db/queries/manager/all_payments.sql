203f2495b7eb79544976fa18863a817c66a0d104
Asal Khaef <asal.khaef@gmail.com>
2024-01-29 11:56:54 +0330

    Add payment queries to manager view

---
+++ server/db/queries/manager/all_payments.sql
@@ -0,0 +1,26 @@
+SELECT
+    p.payment_id,
+    p.rental_id,
+    p.payment_date,
+    p.amount,
+    r.customer_id,
+    r.dvd_id,
+    r.rental_date,
+    r.due_date,
+    r.return_date,
+    r.status,
+    r.rate,
+    r.last_update,
+    s.shop_id,
+    s.shop_name,
+    s.manager_id
+FROM
+    payment p
+JOIN
+    rental r ON p.rental_id = r.rental_id
+JOIN
+    dvd d ON r.dvd_id = d.dvd_id
+JOIN
+    shop s ON d.shop_id = s.shop_id
+WHERE
+    s.manager_id = 6;
